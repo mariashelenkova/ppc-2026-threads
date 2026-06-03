4c7e25d962d3cc1a88fb92de30fbf83ad091214b
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 21:13:17 +0300

    Update ops_stl.hpp

--- tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdio>
+#include <vector>
+
 #include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "task/include/task.hpp"
 
@@ -11,6 +14,10 @@ class ShelenkovaMShellSortSimpleMergeSTL : public BaseTask {
     return ppc::task::TypeOfTask::kSTL;
   }
   explicit ShelenkovaMShellSortSimpleMergeSTL(const InType &in);
+
+  static void BaseShellSort(std::vector<int>::iterator first, std::vector<int>::iterator last);
+  static void ShellSortMerge(std::vector<int> &vec);
+  static std::vector<std::size_t> GetBounds(std::size_t n, std::size_t parts);
 
  private:
   bool ValidationImpl() override;
