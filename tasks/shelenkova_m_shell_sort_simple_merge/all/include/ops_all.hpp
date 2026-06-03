ccc511c25acd80cf5d6f330ae404e7d08de8ddf3
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 20:05:00 +0300

    Update ops_all.hpp

--- tasks/shelenkova_m_shell_sort_simple_merge/all/include/ops_all.hpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/all/include/ops_all.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
+#include <cstdio>
+#include <vector>
+
 #include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "task/include/task.hpp"
 
 namespace shelenkova_m_shell_sort_simple_merge {
 
-class ShelenkovaMShellSortSimpleMergeTBB : public BaseTask {
+class ShelenkovaMShellSortSimpleMergeALL : public BaseTask {
  public:
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-    return ppc::task::TypeOfTask::kTBB;
+    return ppc::task::TypeOfTask::kALL;
   }
-  explicit ShelenkovaMShellSortSimpleMergeTBB(const InType &in);
+  explicit ShelenkovaMShellSortSimpleMergeALL(const InType &in);
+
+  static void GapBasedSort(std::vector<int>::iterator start_pos, std::vector<int>::iterator end_pos);
+  static void SimpleMergeSort(std::vector<int> &arr);
+  static std::vector<std::size_t> CalculateChunks(std::size_t total_len, std::size_t chunk_cnt);
 
  private:
   bool ValidationImpl() override;
