2107d198f7934f82fa9ed6eb1ed693a49eb4cf4a
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 17:23:15 +0300

    Update ops_stl.hpp

--- tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
@@ -8,6 +8,7 @@ namespace shelenkova_m_shell_sort_simple_merge {
 class ShelenkovaMShellSortSimpleMergeSTL : public ppc::task::BaseTask {
  public:
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() { return ppc::task::TypeOfTask::kSTL; }
+
   explicit ShelenkovaMShellSortSimpleMergeSTL(const InType& in);
 
  private:
