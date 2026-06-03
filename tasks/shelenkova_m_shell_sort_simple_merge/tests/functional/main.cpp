c7b815e0e472f80a9068238c4d164d28d0385964
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 16:20:18 +0300

    Update main.cpp

--- tasks/shelenkova_m_shell_sort_simple_merge/tests/functional/main.cpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/tests/functional/main.cpp
@@ -10,6 +10,7 @@
 #include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "shelenkova_m_shell_sort_simple_merge/omp/include/ops_omp.hpp"
 #include "shelenkova_m_shell_sort_simple_merge/seq/include/ops_seq.hpp"
+#include "shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp"
 #include "shelenkova_m_shell_sort_simple_merge/tbb/include/ops_tbb.hpp"
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
@@ -79,11 +80,13 @@ const std::array<TestType, 12> kTestParam = []() {
   return arr;
 }();
 
-const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeOMP, InType>(
+const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSEQ, InType>(
                                                kTestParam, PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
-                                           ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSEQ, InType>(
+                                           ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeOMP, InType>(
                                                kTestParam, PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
                                            ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeTBB, InType>(
+                                               kTestParam, PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
+                                           ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSTL, InType>(
                                                kTestParam, PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
