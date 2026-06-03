bf48b115c54c4309f33cefb14326af3bb38e4ad6
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 17:43:54 +0300

    Update ops_stl.hpp

--- tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
@@ -1,21 +1,25 @@
 #pragma once
 
+#include <vector>
+
 #include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "task/include/task.hpp"
 
 namespace shelenkova_m_shell_sort_simple_merge {
 
-class ShelenkovaMShellSortSimpleMergeSTL : public ppc::task::BaseTask {
+class ShelenkovaMShellSortSimpleMergeSTL : public ppc::task::TaskSTL {
  public:
-  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() { return ppc::task::TypeOfTask::kSTL; }
+  explicit ShelenkovaMShellSortSimpleMergeSTL(std::shared_ptr<ppc::task::TaskData> task_data);
+  ~ShelenkovaMShellSortSimpleMergeSTL() override = default;
 
-  explicit ShelenkovaMShellSortSimpleMergeSTL(const InType& in);
+  bool validation() override;
+  bool pre_processing() override;
+  bool run() override;
+  bool post_processing() override;
 
  private:
-  bool ValidationImpl() override;
-  bool PreProcessingImpl() override;
-  bool RunImpl() override;
-  bool PostProcessingImpl() override;
+  std::vector<int> input_;
+  std::vector<int> output_;
 };
 
 }  // namespace shelenkova_m_shell_sort_simple_merge
