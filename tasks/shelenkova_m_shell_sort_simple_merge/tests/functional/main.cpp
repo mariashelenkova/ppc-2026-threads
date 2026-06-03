49b95483f8b3bf7c99c2db9b021391a73a79bb89
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 18:32:47 +0300

    Update main.cpp

--- tasks/shelenkova_m_shell_sort_simple_merge/tests/functional/main.cpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/tests/functional/main.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <array>
 #include <cstddef>
-#include <random>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -14,89 +13,88 @@
 #include "shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp"
 #include "shelenkova_m_shell_sort_simple_merge/tbb/include/ops_tbb.hpp"
 #include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
 
 namespace shelenkova_m_shell_sort_simple_merge {
 
-struct SortingTestCase {
-  std::vector<int> data;
-  std::string description;
+struct StaticTestCase {
+  std::vector<int> input;
+  std::string name;
 };
 
-const std::array<SortingTestCase, 14> kTestScenarios = {
-    SortingTestCase{.data = std::vector<int>{}, .description = "empty"},
-    SortingTestCase{.data = std::vector<int>{1}, .description = "single_element"},
-    SortingTestCase{.data = std::vector<int>{2, 1}, .description = "two_unsorted"},
-    SortingTestCase{.data = std::vector<int>{1, 2, 3, 4, 5}, .description = "ascending_order"},
-    SortingTestCase{.data = std::vector<int>{5, 4, 3, 2, 1}, .description = "descending_order"},
-    SortingTestCase{.data = std::vector<int>{5, 3, 8, 2, 1, 7, 4, 6}, .description = "random_sequence"},
-    SortingTestCase{.data = std::vector<int>{7, 7, 7, 7, 7}, .description = "identical_values"},
-    SortingTestCase{.data = std::vector<int>{-5, -1, -10, -3, -2, -8, -4}, .description = "negative_numbers"},
-    SortingTestCase{.data = std::vector<int>{-3, 0, 5, -1, 2, -2, 4, -4}, .description = "mixed_values"},
-    SortingTestCase{.data = std::vector<int>{10, -10, 10, -10, 0, 5, -5}, .description = "alternating_signs"},
-    SortingTestCase{.data = std::vector<int>{1000, 500, 250, 125, 62, 31, 15, 7, 3, 1}, .description = "geometric_progression"},
-    SortingTestCase{.data = std::vector<int>{42, -42, 17, 0, -1, 999, -999, 5, -5}, .description = "extreme_range"},
-    SortingTestCase{.data = std::vector<int>{1, 3, 2, 4, 6, 5, 8, 7, 9, 10}, .description = "almost_sorted"},
-    SortingTestCase{.data = std::vector<int>{100, 90, 80, 70, 60, 50, 40, 30, 20, 10}, .description = "large_step_descending"},
+const std::array<StaticTestCase, 12> kStaticTestCases = {
+    StaticTestCase{.input = std::vector<int>{}, .name = "empty"},
+    StaticTestCase{.input = std::vector<int>{1}, .name = "single"},
+    StaticTestCase{.input = std::vector<int>{2, 1}, .name = "two_unsorted"},
+    StaticTestCase{.input = std::vector<int>{1, 2, 3, 4, 5}, .name = "already_sorted"},
+    StaticTestCase{.input = std::vector<int>{5, 4, 3, 2, 1}, .name = "reverse_sorted"},
+    StaticTestCase{.input = std::vector<int>{5, 3, 8, 2, 1}, .name = "small_random"},
+    StaticTestCase{.input = std::vector<int>{7, 7, 7, 7, 7}, .name = "all_equal"},
+    StaticTestCase{.input = std::vector<int>{-5, -1, -10, -3, -2}, .name = "all_negative"},
+    StaticTestCase{.input = std::vector<int>{-3, 0, 5, -1, 2, -2}, .name = "mixed_signs"},
+    StaticTestCase{.input = std::vector<int>{10, -10, 10, -10, 0}, .name = "duplicates_with_zero"},
+    StaticTestCase{.input = std::vector<int>{9, 1, 8, 2, 7, 3, 6, 4, 5}, .name = "zigzag"},
+    StaticTestCase{.input = std::vector<int>{1000, 500, 250, 125, 62, 31, 15, 7, 3, 1}, .name = "halving"},
 };
 
-class SortingFunctionalTest : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class ShelenkovaMRunFuncTestsShellSort : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
-  static std::string GenerateTestLabel(const TestType& parameters) {
-    return std::to_string(std::get<0>(parameters)) + "_" + std::get<1>(parameters);
+  static std::string PrintTestParam(const TestType& test_param) {
+    return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
   }
 
  protected:
   void SetUp() override {
     TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    const size_t scenario_index = std::get<0>(params);
-    original_data_ = kTestScenarios.at(scenario_index).data;
+    const size_t idx = std::get<0>(params);
+    input_data_ = kStaticTestCases.at(idx).input;
 
-    expected_output_ = original_data_;
-    std::sort(expected_output_.begin(), expected_output_.end());
+    expected_result_ = input_data_;
+    std::sort(expected_result_.begin(), expected_result_.end());
   }
 
-  bool ValidateOutput(OutType& result) final {
-    return result == expected_output_;
+  bool CheckTestOutputData(OutType& output_data) final {
+    return output_data == expected_result_;
   }
 
-  InType FetchInputData() final {
-    return original_data_;
+  InType GetTestInputData() final {
+    return input_data_;
   }
 
  private:
-  InType original_data_;
-  OutType expected_output_;
+  InType input_data_;
+  OutType expected_result_;
 };
-
-TEST_P(SortingFunctionalTest, ShellSortValidation) {
-  ExecuteTest(GetParam());
-}
 
 namespace {
 
-const std::array<TestType, 14> kTestParameters = []() {
-  std::array<TestType, 14> params;
-  for (size_t i = 0; i < params.size(); ++i) {
-    params.at(i) = std::make_tuple(i, kTestScenarios.at(i).description);
+TEST_P(ShelenkovaMRunFuncTestsShellSort, ShellSortSimpleMerge) {
+  ExecuteTest(GetParam());
+}
+
+const std::array<TestType, 12> kTestParam = []() {
+  std::array<TestType, 12> arr;
+  for (size_t i = 0; i < arr.size(); ++i) {
+    arr.at(i) = std::make_tuple(i, kStaticTestCases.at(i).name);
   }
-  return params;
+  return arr;
 }();
 
-const auto kAllTestTasks = std::tuple_cat(
-    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSEQ, InType>(kTestParameters,
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSEQ, InType>(kTestParam,
                                                                         PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
-    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeOMP, InType>(kTestParameters,
+    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeOMP, InType>(kTestParam,
                                                                         PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
-    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeTBB, InType>(kTestParameters,
+    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeTBB, InType>(kTestParam,
                                                                         PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge),
-    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSTL, InType>(kTestParameters,
+    ppc::util::AddFuncTask<ShelenkovaMShellSortSimpleMergeSTL, InType>(kTestParam,
                                                                         PPC_SETTINGS_shelenkova_m_shell_sort_simple_merge));
 
-const auto kGtestValues = ppc::util::ExpandToValues(kAllTestTasks);
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kTestSuiteName = SortingFunctionalTest::PrintFuncTestName<SortingFunctionalTest>;
+const auto kPerfTestName = ShelenkovaMRunFuncTestsShellSort::PrintFuncTestName<ShelenkovaMRunFuncTestsShellSort>;
 
-INSTANTIATE_TEST_SUITE_P(ShellSortFunctionalTests, SortingFunctionalTest, kGtestValues, kTestSuiteName);
+INSTANTIATE_TEST_SUITE_P(ShellSortTests, ShelenkovaMRunFuncTestsShellSort, kGtestValues, kPerfTestName);
 
 }  // namespace
 
