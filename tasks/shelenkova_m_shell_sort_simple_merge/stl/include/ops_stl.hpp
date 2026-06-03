7e7bc035703f92f8160bf76f4309db7202eb0c2a
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 18:22:23 +0300

    Update ops_stl.hpp

--- tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/stl/include/ops_stl.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstddef>
+#include <vector>
+
 #include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
 #include "task/include/task.hpp"
 
@@ -7,10 +10,12 @@ namespace shelenkova_m_shell_sort_simple_merge {
 
 class ShelenkovaMShellSortSimpleMergeSTL : public BaseTask {
  public:
-  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-    return ppc::task::TypeOfTask::kSTL;
-  }
-  explicit ShelenkovaMShellSortSimpleMergeSTL(const InType &in);
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() { return ppc::task::TypeOfTask::kSTL; }
+  explicit ShelenkovaMShellSortSimpleMergeSTL(const InType& in);
+
+  static void SortingProcedure(std::vector<int>::iterator start, std::vector<int>::iterator finish);
+  static void FullSorting(std::vector<int>& container);
+  static std::vector<std::size_t> PartitionIndices(std::size_t total, std::size_t segments);
 
  private:
   bool ValidationImpl() override;
