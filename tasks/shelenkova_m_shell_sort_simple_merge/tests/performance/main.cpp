ba180e8f249063233a7aae0daf8f3f470b56c735
mariashelenkova <144237865+mariashelenkova@users.noreply.github.com>
2026-06-03 18:40:53 +0300

    Update main.cpp

--- tasks/shelenkova_m_shell_sort_simple_merge/tests/performance/main.cpp
+++ tasks/shelenkova_m_shell_sort_simple_merge/tests/performance/main.cpp
@@ -56,14 +56,10 @@ class ShelenkovaMRunPerfTestShellSort : public ppc::util::BaseRunPerfTests<InTyp
     return std::is_sorted(output_data.begin(), output_data.end());
   }
 
-  InType GetTestInputData() final {
-    return input_data_;
-  }
+  InType GetTestInputData() final { return input_data_; }
 };
 
-TEST_P(ShelenkovaMRunPerfTestShellSort, RunPerfShellSort) {
-  ExecuteTest(GetParam());
-}
+TEST_P(ShelenkovaMRunPerfTestShellSort, RunPerfShellSort) { ExecuteTest(GetParam()); }
 
 namespace {
 
